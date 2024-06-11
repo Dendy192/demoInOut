eed98c58ca197e95dfc2993763be51419a6ebbae
dendy192 <dtiawan192@gmail.com>
2024-06-12 01:26:25 +0700

    first commit

---
+++ src/main/java/com/dendy/countinout/CountInOutApplication.java
@@ -0,0 +1,13 @@
+package com.dendy.countinout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountInOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountInOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dendy/countinout/CountInOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dendy.countinout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountInOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
